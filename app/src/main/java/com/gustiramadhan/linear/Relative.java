c7b36e3bc23287c7a86689410de6b327c2a3d6cb
gusti-code <gustiramadhan830@gmail.com>
2019-10-16 11:18:25 +0700

    linearanadralatuve

---
+++ app/src/main/java/com/gustiramadhan/linear/Relative.java
@@ -0,0 +1,14 @@
+package com.gustiramadhan.linear;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Relative extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative);
+    }
+}
